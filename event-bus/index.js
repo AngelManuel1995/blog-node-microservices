dd1b0a7c886fcde0d0753aee2caa08d73c00ad81
Angel Manuel Góez Giraldo <angelmanuel.goez@hotmail.es>
2021-04-05 20:52:41 -0500

    Changing localhost for service kubernetes name post-clusterip-srv

--- event-bus/index.js
+++ event-bus/index.js
@@ -11,10 +11,10 @@ app.use(express.json())
 app.post('/events', (req, res) => {
     const { body:event } = req
     events.push(event)
-    axios.post('http://localhost:4000/events', event)
-    axios.post('http://localhost:4001/events', event)
-    axios.post('http://localhost:4002/events', event)
-    axios.post('http://localhost:4003/events', event)
+    axios.post('http://post-clusterip-srv:4000/events', event)
+    // axios.post('http://localhost:4001/events', event)
+    // axios.post('http://localhost:4002/events', event)
+    // axios.post('http://localhost:4003/events', event)
     res.send({status:'OK'})
 })
 
