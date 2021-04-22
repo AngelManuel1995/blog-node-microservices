0c9ff1f019355da91f85471bf1775f50dd3f3930
Angel Manuel Góez Giraldo <angelmanuel.goez@hotmail.es>
2021-04-21 22:25:05 -0500

    event pointing to difetent services

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,9 +12,9 @@ app.post('/events', (req, res) => {
     const { body:event } = req
     events.push(event)
     axios.post('http://post-clusterip-srv:4000/events', event)
-    // axios.post('http://localhost:4001/events', event)
-    // axios.post('http://localhost:4002/events', event)
-    // axios.post('http://localhost:4003/events', event)
+    axios.post('http://comments-srv:4001/events', event)
+    axios.post('http://query-srv:4002/events', event)
+    axios.post('http://moderation-srv:4003/events', event)
     res.send({status:'OK'})
 })
 
