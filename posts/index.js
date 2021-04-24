5984958749cc64ed41936debb540853519fb8eff
Angel Manuel Góez Giraldo <angelmanuel.goez@hotmail.es>
2021-04-23 22:16:19 -0500

    Project is working correctly

--- posts/index.js
+++ posts/index.js
@@ -11,7 +11,7 @@ app.use(cors())
 const PORT = process.env.PORT || 4000
 const posts = {}
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
 	const id = randomBytes(4).toString('hex')
 	const { title } = req.body
 	posts[id] = {
